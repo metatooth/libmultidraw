81339bcf8339133fee23ae8f1e8176a18958d087
Terry <tgl@rideside.net>
2023-03-12 13:10:11 -0400

    first attempt at a smoke test/example

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,16 @@
-from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
-
+from conans import ConanFile, tools, CMake
+from conan.tools.cmake import CMakeToolchain, cmake_layout, CMakeDeps
 
 class MultidrawConan(ConanFile):
     name = "libmultidraw"
     version = "0.1"
-    requires = "doxygen/1.9.4", "fltk/1.3.8"
-    generators = "cmake"
+
+    generators = "cmake", "cmake_find_package"
     
     # Optional metadata
     license = "MIT"
     author = "Terry Lorber terry@metatooth.com"
-    url = "https://github.com/metatooth/libmultidraw"
+    url = "https://github.com/metatooth/multidraw"
     description = "A multi-platform framework for domain-specific editors."
     topics = ("framework", "design", "visualization", "2D", "3D")
 
@@ -20,8 +19,9 @@ class MultidrawConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
 
-    # Sources are located in the same place as this recipe, copy them to the recipe
-    exports_sources = "CMakeLists.txt", "libmultidraw/*"
+    # Sources are located in the same place as this recipe, copy them
+    # to the recipe
+    exports_sources = "CMakeLists.txt", "VERSION", "libmultidraw/*", "doc/*"
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -30,8 +30,16 @@ class MultidrawConan(ConanFile):
     def layout(self):
         cmake_layout(self)
 
+    def requirements(self):
+        self.requires("fltk/1.3.8")
+        self.requires("doxygen/1.9.4")
+        self.requires("libxft/2.3.6")
+
     def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
         tc = CMakeToolchain(self)
+        tc.user_presets_path = "ConanPresets.json"
         tc.generate()
 
     def build(self):
